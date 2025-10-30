f8eb89a8024419aaf10a26e7678a4b08f29c71fa
jaemarmorrisramos-cpu <jaemarmorrisramos@gmail.com>
2025-10-30 15:54:33 +0800

    Add files via upload

---
+++ Basketball-Scoreboard/index.js
@@ -0,0 +1,30 @@
+let homeScore = 0
+let guestScore = 0
+
+function increment() {
+    homeScore += 1
+    document.getElementById("home-el").textContent = homeScore
+}
+function increment2() {
+    homeScore += 2
+    document.getElementById("home-el").textContent = homeScore
+}
+function increment3() {
+    homeScore += 3
+    document.getElementById("home-el").textContent = homeScore
+}
+
+function incrementg() {
+    guestScore += 1
+    document.getElementById("guest-el").textContent = guestScore
+}
+
+function incrementg2() {
+    guestScore += 1
+    document.getElementById("guest-el").textContent = guestScore
+}
+
+function incrementg3() {
+    guestScore += 1
+    document.getElementById("guest-el").textContent = guestScore
+}
